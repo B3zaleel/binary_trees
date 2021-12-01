bd05779428743eca85bcfa0dbcd93b2a066b0805
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-02 00:52:08 +0100

    Update AVL balance checking algorithm

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -27,12 +27,12 @@ void bst_checker(const binary_tree_t *tree, int min, int max, int *is_bst)
 }
 
 /**
- * tree_height_c - Computes the height of a binary tree.
+ * tree_height_1 - Computes the height of a binary tree.
  * @tree: The binary tree.
  * @n: The accumulated height of the current tree.
  * @height: A pointer to the tree's maximum height value.
  */
-void tree_height_c(const binary_tree_t *tree, int n, int *height)
+void tree_height_1(const binary_tree_t *tree, int n, int *height)
 {
 	if (tree != NULL)
 	{
@@ -45,19 +45,19 @@ void tree_height_c(const binary_tree_t *tree, int n, int *height)
 		}
 		else
 		{
-			tree_height_c(tree->left, n + 1, height);
-			tree_height_c(tree->right, n + 1, height);
+			tree_height_1(tree->left, n + 1, height);
+			tree_height_1(tree->right, n + 1, height);
 		}
 	}
 }
 
 /**
- * bin_tree_balance - Computes the balance factor of a binary tree.
+ * avl_tree_balance - Recursively checks the balance of a binary tree and \
+ * its nodes and sets the balanced flag to 0 if it isn't all balanced.
  * @tree: The binary tree.
- *
- * Return: The balance factor of the given binary tree.
+ * @is_balanced: A pointer to the balanced flag's value.
  */
-int bin_tree_balance(const binary_tree_t *tree)
+void avl_tree_balance(const binary_tree_t *tree, int *is_balanced)
 {
 	int balance_factor = 0;
 	int left_height = 0;
@@ -65,11 +65,22 @@ int bin_tree_balance(const binary_tree_t *tree)
 
 	if (tree != NULL)
 	{
-		tree_height_c(tree->left, 1, &left_height);
-		tree_height_c(tree->right, 1, &right_height);
+		tree_height_1(tree->left, 1, &left_height);
+		tree_height_1(tree->right, 1, &right_height);
+		balance_factor = left_height - right_height;
+		if (!((balance_factor >= -1) && (balance_factor <= 1)))
+		{
+			if (is_balanced != NULL)
+			{
+				*is_balanced = 0;
+			}
+		}
+		else
+		{
+			avl_tree_balance(tree->left, is_balanced);
+			avl_tree_balance(tree->right, is_balanced);
+		}
 	}
-	balance_factor = left_height - right_height;
-	return (balance_factor);
 }
 
 /**
@@ -80,7 +91,6 @@ int bin_tree_balance(const binary_tree_t *tree)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int diff = 0;
 	int is_bst = 0;
 	int is_avl = 0;
 
@@ -90,11 +100,8 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 		bst_checker(tree, INT_MIN, INT_MAX, &is_bst);
 		if (is_bst == 1)
 		{
-			diff = bin_tree_balance(tree->left) - bin_tree_balance(tree->right);
-			if ((diff >= -1) && (diff <= 1))
-			{
-				is_avl = 1;
-			}
+			is_avl = 1;
+			avl_tree_balance(tree, &is_avl);
 		}
 	}
 	return (is_avl);
