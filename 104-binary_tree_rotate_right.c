c52defb1c6a761f7596d80c00b79877d24a266a9
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-02 06:56:21 +0100

    Update tree rotation algorithms

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,31 +8,40 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *root;
-	binary_tree_t *new_root;
-	binary_tree_t *new_root_rc;
+	binary_tree_t *root,  *new_root, *new_root_rc, *root_p;
 
 	if (tree == NULL)
 		return (NULL);
-	root = tree;
-	new_root = root->left;
-	if (new_root != NULL)
+	root = tree, root_p = root->parent;
+	if ((root->left == NULL) && (root->right == NULL))
 	{
-		new_root->parent = root->parent;
-		new_root_rc = new_root->right;
-		new_root->right = root;
-		if (root->parent != NULL)
+		if ((root->parent != NULL) && (root->parent->left == root))
 		{
-			if (root->parent->left == root)
-				root->parent->left = new_root;
-			if (root->parent->right == root)
-				root->parent->right = new_root;
+			if (root_p->parent != NULL && (root_p->parent->left == root_p))
+				root_p->parent->left = root;
+			if (root_p->parent != NULL && (root_p->parent->right == root_p))
+				root_p->parent->right = root;
+			root->parent = root_p->parent;
+			root_p->left = NULL, root_p->parent = root;
+			root->right = root_p;
 		}
-		root->left = new_root_rc;
-		root->parent = new_root;
-		if (new_root_rc != NULL)
+	}
+	else
+	{
+		if (root->left != NULL)
 		{
-			new_root_rc->parent = root;
+			new_root = root->left, new_root->parent = root->parent;
+			new_root_rc = new_root->right, new_root->right = root;
+			if (root->parent != NULL)
+			{
+				if (root->parent->left == root)
+					root->parent->left = new_root;
+				if (root->parent->right == root)
+					root->parent->right = new_root;
+			}
+			root->left = new_root_rc, root->parent = new_root;
+			if (new_root_rc != NULL)
+				new_root_rc->parent = root;
 		}
 	}
 	return (new_root);
