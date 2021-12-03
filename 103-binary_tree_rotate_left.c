def13f3ae2b31e62e09759c2b2e3ea9184ffe2d6
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-03 05:26:50 +0100

    Update rotation algorithms

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,6 +1,50 @@
 #include "binary_trees.h"
 
 /**
+ * rotate_left_with_child - Rotates a tree with its children to the left.
+ * @tree: A pointer to the node to rotate.
+ *
+ * Return: The new root node.
+ */
+binary_tree_t *rotate_left_with_child(binary_tree_t *tree)
+{
+	binary_tree_t *root, *new_root = tree, *new_root_lc, *root_p, *root_l;
+
+	root = tree, root_p = root->parent;
+	if (root_p == NULL)
+	{
+		if (root->right != NULL)
+		{
+			new_root = root->right;
+			new_root->parent = root->parent;
+			new_root_lc = new_root->left, new_root->left = root;
+			root->right = new_root_lc, root->parent = new_root;
+			if (new_root_lc != NULL)
+				new_root_lc->parent = root;
+		}
+	}
+	else
+	{
+		if (root_p->right == root)
+		{
+			if ((root_p->parent != NULL) && (root_p->parent->left == root_p))
+				root_p->parent->left = root;
+			if ((root_p->parent != NULL) && (root_p->parent->right == root_p))
+				root_p->parent->right = root;
+			root_l = root->left;
+			root->left = root_p;
+			root->parent = root_p->parent;
+			root_p->right = root_l;
+			root_p->parent = root;
+			if (root_l != NULL)
+				root_l->parent = root_p;
+			new_root = (root->parent == NULL ? root : new_root);
+		}
+	}
+	return (new_root);
+}
+
+/**
  * binary_tree_rotate_left - Rotates a node to the left.
  * @tree: A pointer to the node to rotate.
  *
@@ -8,41 +52,40 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *root, *root_p, *root_r, *new_root, *new_root_lc;
+	binary_tree_t *root, *root_p, *new_root;
 
 	if (tree == NULL)
 		return (NULL);
-	root = tree, root_r = root->right, root_p = root->parent, new_root = root;
+	root = tree, root_p = root->parent, new_root = root;
 	if ((root->left == NULL) && (root->right == NULL))
 	{
-		if ((root->parent != NULL) && (root->parent->right == root))
+		if (root->parent != NULL)
 		{
-			if ((root_p->parent != NULL) && (root_p->parent->right == root_p))
-				root_p->parent->right = root;
-			if ((root_p->parent != NULL) && (root_p->parent->left == root_p))
-				root_p->parent->left = root;
-			root->parent = root_p->parent;
-			root_p->right = NULL, root_p->parent = root;
-			root->left = root_p;
+			if (root->parent->right == root)
+			{
+				if ((root_p->parent != NULL) && (root_p->parent->right == root_p))
+					root_p->parent->right = root;
+				if ((root_p->parent != NULL) && (root_p->parent->left == root_p))
+					root_p->parent->left = root;
+				root->parent = root_p->parent;
+				root_p->right = NULL, root_p->parent = root;
+				root->left = root_p;
+			}
+			else
+			{
+				if ((root_p->parent != NULL) && (root_p->parent->right == root_p))
+					root_p->parent->right = root;
+				if ((root_p->parent != NULL) && (root_p->parent->left == root_p))
+					root_p->parent->left = root;
+				root->parent = root_p->parent;
+				root_p->left = NULL, root_p->parent = root;
+				root->right = root_p;
+			}
 		}
 	}
 	else
 	{
-		if (root_r != NULL)
-		{
-			new_root = root_r, new_root->parent = root->parent;
-			new_root_lc = new_root->left, new_root->left = root;
-			if (root->parent != NULL)
-			{
-				if (root->parent->left == root)
-					root->parent->left = new_root;
-				if (root->parent->right == root)
-					root->parent->right = new_root;
-			}
-			root->right = new_root_lc, root->parent = new_root;
-			if (new_root_lc != NULL)
-				new_root_lc->parent = root;
-		}
+		new_root = rotate_left_with_child(tree);
 	}
 	return (new_root);
 }
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,6 +1,79 @@
 #include "binary_trees.h"
 
 /**
+ * rotate_right_with_parent - Rotates a tree with its parent to the right.
+ * @tree: A pointer to the node to rotate.
+ *
+ * Return: The new root node.
+ */
+binary_tree_t *rotate_right_with_parent(binary_tree_t *tree)
+{
+	binary_tree_t *root, *new_root = tree, *new_root_rc, *root_p, *root_r;
+
+	root = tree, root_p = root->parent;
+	(void)new_root_rc;
+	if (root_p->left == root)
+	{
+		if ((root_p->parent != NULL) && (root_p->parent->left == root_p))
+			root_p->parent->left = root;
+		if ((root_p->parent != NULL) && (root_p->parent->right == root_p))
+			root_p->parent->right = root;
+		root_r = root->right;
+		root_p->left = root_r;
+		root_p->parent = root;
+		root->right = root_p;
+		root->parent = root_p->parent;
+		(void)root_r;
+		if (root_r != NULL)
+			root_r->parent = root_p;
+		new_root = (root->parent == NULL ? root : new_root);
+	}
+	else
+	{
+		if ((root_p->parent != NULL) && (root_p->parent->left == root_p))
+			root_p->parent->left = root;
+		if ((root_p->parent != NULL) && (root_p->parent->right == root_p))
+			root_p->parent->right = root;
+		root_r = root->right;
+		root_p->left = root_r;
+		root_p->parent = root;
+		root->right = root_p;
+		root->parent = root_p->parent;
+		(void)root_r;
+		if (root_r != NULL)
+			root_r->parent = root_p;
+		new_root = (root->parent == NULL ? root : new_root);
+	}
+	return (new_root);
+}
+
+/**
+ * rotate_right_with_child - Rotates a tree with its children to the right.
+ * @tree: A pointer to the node to rotate.
+ *
+ * Return: The new root node.
+ */
+binary_tree_t *rotate_right_with_child(binary_tree_t *tree)
+{
+	binary_tree_t *root, *new_root = tree, *new_root_rc, *root_p;
+
+	root = tree, root_p = root->parent;
+	if (root_p == NULL)
+	{
+		new_root = root->left, new_root->parent = root->parent;
+		new_root_rc = new_root->right, new_root->right = root;
+		root->left = new_root_rc, root->parent = new_root;
+		if (new_root_rc != NULL)
+			new_root_rc->parent = root;
+	}
+	else
+	{
+		new_root = rotate_right_with_parent(root);
+	}
+	return (new_root);
+}
+
+/**
  * binary_tree_rotate_right - Rotates a node to the right.
  * @tree: A pointer to the node to rotate.
  *
@@ -8,41 +81,40 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *root,  *new_root, *new_root_rc, *root_p;
+	binary_tree_t *root, *new_root = tree, *root_p;
 
 	if (tree == NULL)
 		return (NULL);
 	root = tree, root_p = root->parent;
 	if ((root->left == NULL) && (root->right == NULL))
 	{
-		if ((root->parent != NULL) && (root->parent->left == root))
+		if (root->parent != NULL)
 		{
-			if (root_p->parent != NULL && (root_p->parent->left == root_p))
-				root_p->parent->left = root;
-			if (root_p->parent != NULL && (root_p->parent->right == root_p))
-				root_p->parent->right = root;
-			root->parent = root_p->parent;
-			root_p->left = NULL, root_p->parent = root;
-			root->right = root_p;
+			if (root->parent->left == root)
+			{
+				if (root_p->parent != NULL && (root_p->parent->left == root_p))
+					root_p->parent->left = root;
+				if (root_p->parent != NULL && (root_p->parent->right == root_p))
+					root_p->parent->right = root;
+				root->parent = root_p->parent;
+				root_p->left = NULL, root_p->parent = root;
+				root->right = root_p;
+			}
+			else
+			{
+				if (root_p->parent != NULL && (root_p->parent->left == root_p))
+					root_p->parent->left = root;
+				if (root_p->parent != NULL && (root_p->parent->right == root_p))
+					root_p->parent->right = root;
+				root->parent = root_p->parent;
+				root_p->right = NULL, root_p->parent = root;
+				root->left = root_p;
+			}
 		}
 	}
 	else
 	{
-		if (root->left != NULL)
-		{
-			new_root = root->left, new_root->parent = root->parent;
-			new_root_rc = new_root->right, new_root->right = root;
-			if (root->parent != NULL)
-			{
-				if (root->parent->left == root)
-					root->parent->left = new_root;
-				if (root->parent->right == root)
-					root->parent->right = new_root;
-			}
-			root->left = new_root_rc, root->parent = new_root;
-			if (new_root_rc != NULL)
-				new_root_rc->parent = root;
-		}
+		new_root = rotate_right_with_child(tree);
 	}
 	return (new_root);
 }
